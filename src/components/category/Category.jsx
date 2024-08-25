d2b5e686884f4e09cfac8972325485878f688989
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-08-25 14:47:51 +0500

    category

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,12 +1,23 @@
 import React, { memo } from 'react'
+import { useFetch } from '../../hooks/useFetch'
 
-const Category = ({setCategory, data}) => {
+
+const Category = ({ setCategory }) => {
+  const { data } = useFetch("/products/category-list")
+  console.log(data);
+
+  const laylo = data?.map((element) => (
+    <p className='text'>{element}</p>
+
+  ))
 
 
   return (
     <>
       <div className="container">
-   
+        <div className='flex gap-4 overflow-auto flex-wrap'>
+           {laylo}
+        </div>
       </div>
     </>
   )
